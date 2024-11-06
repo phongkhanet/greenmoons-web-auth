242148ec4c278e287f97e4b56eaf8d1002aaf9d1
phongkhanet <phongkhanet@greenmoons.co.th>
2024-11-06 16:08:17 +0700

    updated: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 import { resolve } from "path";
-
 import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [
+    react(),
+    dts({
+      include: ["lib"],
+      outDir: "dist", // Ensure types are output to the dist folder
+      tsconfigPath: "./tsconfig-build.json", // Specify the tsconfig for type generation
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
+      fileName: "greenmoons-web-auth", // Set file name without the .js extension
+    },
+    outDir: "dist", // Ensure everything goes to the dist folder
+    rollupOptions: {
+      external: ["react", "react-dom"], // Mark peer dependencies as external
     },
   },
 });
